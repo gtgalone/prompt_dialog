1da73ba6a54f62d4f308436f39c8146a1873ea7f
gtgalone <sjhgenius@gmail.com>
2020-06-15 04:42:55 -0400

    Add api document and will pop scope

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 # 0.0.1
 
   * Publish initial version.
+
+# 0.0.2
+
+  * Add api document and will pop scope.
--- lib/prompt_dialog.dart
+++ lib/prompt_dialog.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+/// The `message` argument is used to title of dialog.
+/// The `initialValue` argument is used to initial value of dialog.
+/// The `textOK` argument is used to text for 'OK' Button of dialog.
+/// The `textCancel` argument is used to text for 'Cancel' Button of dialog.
+/// The `autoFocus` argument will auto focus text field form of dialog.
+///
+/// Returns a [Future<bool>].
 Future<String> prompt(
   BuildContext context,
   {
@@ -13,17 +20,20 @@ Future<String> prompt(
   String value;
   return showDialog(
     context: context,
-    builder: (_) => AlertDialog(
-      title: (message != null) ? Text(message) : null,
-      content: TextFormField(
-        autofocus: autoFocus,
-        initialValue: initialValue,
-        onChanged: (text) => value = text,
+    builder: (_) => WillPopScope(
+      child: AlertDialog(
+        title: (message != null) ? Text(message) : null,
+        content: TextFormField(
+          autofocus: autoFocus,
+          initialValue: initialValue,
+          onChanged: (text) => value = text,
+        ),
+        actions: <Widget>[
+          FlatButton(child: Text(textCancel), onPressed: () => Navigator.pop(context, null)),
+          FlatButton(child: Text(textOK), onPressed: () => Navigator.pop(context, value)),
+        ],
       ),
-      actions: <Widget>[
-        FlatButton(child: Text(textCancel), onPressed: () => Navigator.pop(context, null)),
-        FlatButton(child: Text(textOK), onPressed: () => Navigator.pop(context, value)),
-      ],
+      onWillPop: () { Navigator.pop(context, false); return; },
     ),
   );
 }
